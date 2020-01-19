188aae9cfffe3397e99b63958e84b36aaf3503d4
Arthur Sir. GD1.2 <35264395+CrocoDeals@users.noreply.github.com>
2020-01-19 15:23:49 +0100

    Exercice 3
    Slime contre Heros cette fois.
    Le heros subit une lourde contre-attaque de 10 point de vie.
    Game Over / Le combat s'arrête

--- SystemeDeCombat.c
+++ SystemeDeCombat.c
@@ -7,14 +7,21 @@ int main(){
     int pvHeros = 30;
 		int pvMonstre = 10;
 		int attaqueEpee = 2;
+		int contreAttaque = 10;
 
     printf("Vous avez %d points de vie.\n", pvHeros);
 		printf("Slime a %d points de vie.\n", pvMonstre);
 
-		while (pvMonstre > 0 ) {
+		while (pvMonstre > 0 && pvHeros > 0 ) {
 			printf("Heros lance Coup d'Epee !\nSlime subit %d de degats !\n", attaqueEpee);
 	    pvMonstre = pvMonstre - attaqueEpee;
 			printf("Slime a %d points de vie.\n", pvMonstre);
+			printf("Slime contre-attaque immediatement ! Vous perdez %d points de vie !\n", contreAttaque);
+			pvHeros = pvHeros - contreAttaque;
+			printf("Vous avez %d points de vie.\n", pvHeros);
+				if (pvHeros <= 0) {
+					printf("Vous etes mort. Game Over !\n");
+				}
 				if (pvMonstre <= 0) {
 					printf("Slime est mort !\n");
 				}
